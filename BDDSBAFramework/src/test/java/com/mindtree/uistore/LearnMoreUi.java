6458af355fc913b476d4537c8f1a3cfce3ee2ad9
Gagan Karthik <46991274+KarthikGagan@users.noreply.github.com>
2021-11-09 13:23:24 +0530

    Delete BDDSBAFramework directory

--- BDDSBAFramework/src/test/java/com/mindtree/uistore/LearnMoreUi.java
+++
@@ -1,8 +0,0 @@
-package com.mindtree.uistore;
-
-import org.openqa.selenium.By;
-
-public class LearnMoreUi {
-	public static By learnMore=By.xpath("//a[@data-action-detail='/en-us/how-to-wash-clothes/how-to-do-laundry']");
-	public static By result=By.xpath("//p[contains(text(),'How to Read Laundry Symbols')]");
-}
